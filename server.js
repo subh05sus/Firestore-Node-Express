3fb576a24b846f7c4a6ac80333700a3d185135f5
subh05sus <sahasubhadipfkk@gmail.com>
2023-09-24 01:16:33 +0530

    added openai integrated summary

--- server.js
+++ server.js
@@ -1,105 +1,147 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const admin = require("firebase-admin")
-const credentials = require("./key.json")
+const admin = require("firebase-admin");
+const credentials = require("./key.json");
+
+const OpenAI = require("openai");
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 admin.initializeApp({
-    credential: admin.credential.cert(credentials)
+  credential: admin.credential.cert(credentials),
 });
-const db = admin.firestore()
+const db = admin.firestore();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
+const ejs = require("ejs"); // Require EJS
 
-const PORT = 3000
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+// ...
 
-
-
+app.set("view engine", "ejs"); // Set EJS as the view engine
 
 // --------------------view----------------------------------
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-//-----------------------create------------------------------
-app.post('/create', async (req, res)=>{
-    try {
-        console.log(req.body)
-        const userJson = {
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            age: req.body.age,
-            sex: req.body.sex,
-        };
-        const response = await db.collection("users").add(userJson);
-        res.send(response)
-    } catch (e) {
-        res.send(e)
+// -------------------------read------------------------------
+
+app.post("/read", async (req, res) => {
+  const userId = req.body.id;
+  const userPasskey = req.body.passkey;
+
+  if (!userId || !userPasskey) {
+    res.send("User ID or passkey is missing.");
+    return;
+  }
+
+  try {
+    const userRef = db.collection("PatientData").doc(userId);
+    const response = await userRef.get();
+
+    if (!response.exists) {
+      res.send("User not found");
+      return;
     }
-})
 
-// --------------------------read all----------------------
-app.get('/read/all',  async (req, res)=>{
-    try {
-        const userRef = db.collection("users");
-        const response =  await userRef.get();
-        let responseArr = [];
-        response.forEach(doc => {
-            responseArr.push(doc.data());
-        });
-        res.send(responseArr);
-    } catch (e) {
-        res.send(e);
+    const userData = response.data();
+
+    if (userData.password !== userPasskey) {
+      res.send("Incorrect passkey");
+      return;
     }
-})
 
-// -------------------------read any one------------------------------
-app.get('/read/:id',  async (req, res)=>{
-    try {
-        const userRef = db.collection("users").doc(req.params.id);
-        const response =  await userRef.get();
-        res.send(response.data());
-    } catch (e) {
-        res.send(e);
-    }
-})
+    let caseHistory =
+      "You are a bot who takes case history of a patient and summarizes it into 3-4 sentences that a doctor can easity understand. ONLY PROVIDE THE SUMMARY, NO SUMMARY TITLES\n\nHere's the case history of patient:\n";
+    caseHistory += userData.patientBasicData;
+    userData.cases.forEach((caseItem, index) => {
+      caseHistory += `${index + 1}. ${caseItem.caseTitle}\n${
+        caseItem.description
+      }\n\n`;
+    });
+    caseHistory += "NOW GIVE THE SUMMARY";
 
+    let summaryofPatient = userData.patientBasicData;
+    
+    const summary = async () => {
+      const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "assistant", content: caseHistory }],
+        max_tokens: 100,
+      });
+      summaryofPatient = chatCompletion.choices[0].message.content;
+      console.log(summaryofPatient);
 
-//----------------update--------------------------
-app.post('/update',  async (req, res)=>{
-    try {
-        const id= req.body.id;
-        const newEmail = "taylorislove@gmail.com";
-        const newFirstName = "Taylor";
-        const newLastName = "Swift";
-        const newAge = "16";
-        const newSex = "female";
+      await db.collection("PatientData").doc(userId).update({
+        patientBasicData: summaryofPatient,
+      });
+    };
 
-        const userRef = await db.collection("users").doc(id)
-        .update({
-            email: newEmail,
-            firstName: newFirstName,
-            lastName: newLastName,
-            age: newAge,
-            sex: newSex,
-        })
-        const response =  await userRef.get();
-        res.send(response);
-    } catch (e) {
-        res.send(e);
-    }
-})
+    summary();
 
 
-// --------------------delete-------------------------------
-app.delete('/delete/:id',  async (req, res)=>{
-    try {
-        const response = await db.collection("users").doc(req.params.id).delete();
-        res.send(response);
-    } catch (e) {
-        res.send(e);
-    }
-})
+
+
+
+    res.render("user", {
+      user: userData,
+    });
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+// ---------------------firestore structure------------------------------------------------
+// const userData = {
+//     firstName: "Alice",
+//     lastName: "Smith",
+//     sex: "Female",
+//     age: 25,
+//     phone: "1234567890",
+//     email: "alice@example.com",
+//     password: "securepass",
+//     bloodGroup: "O+",
+//     height: "160cm",
+//     weight: "65kg",
+//     totalCases: 2,
+//     cases: [
+//         {
+//             time: "Monday, 8:00 AM",
+//             caseTitle: "Headache",
+//             description: "Had a headache on Monday morning"
+//         },
+//         {
+//             time: "Wednesday, 3:30 PM",
+//             caseTitle: "Allergy",
+//             description: "Experienced allergy symptoms on Wednesday"
+//         }
+//     ],
+//     firstData: "Allergic to peanuts, otherwise healthy",
+//     patientBasicData:""
+// };
+
+// const userData = {
+//     firstName: "Eva",
+//     lastName: "Williams",
+//     sex: "Female",
+//     age: 28,
+//     phone: "5551234567",
+//     email: "eva@example.com",
+//     password: "mypassword",
+//     bloodGroup: "AB+",
+//     height: "162cm",
+//     weight: "58kg",
+//     totalCases: 0,
+//     cases: [],
+//     patientBasicData: "No recent medical history"
+// };
+
+// db.collection("PatientData").doc(userData.email).set(userData);
